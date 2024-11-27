0ec8c8215bb0b5e735eeb8132f195dc2609b7f69
lpoulin <lucien.poulin@sscinc.com>
2024-11-26 20:22:55 -0500

    (WIP) Adding Job Scheduler support for BullMQ Queues

--- packages/ui/src/services/i18n.ts
+++ packages/ui/src/services/i18n.ts
@@ -1,8 +1,8 @@
 import i18n from 'i18next';
 import HttpBackend from 'i18next-http-backend';
-import * as process from 'process';
 import { initReactI18next } from 'react-i18next';
 import enLocale from 'date-fns/locale/en-US';
+import { languages } from '../constants/languages';
 
 export let dateFnsLocale = enLocale;
 const dateFnsLocaleMap = {
@@ -13,14 +13,19 @@ const dateFnsLocaleMap = {
 async function setDateFnsLocale(lng: string) {
   const languageToLoad = dateFnsLocaleMap[lng as keyof typeof dateFnsLocaleMap] || lng;
   dateFnsLocale = await import(`date-fns/locale/${languageToLoad}/index.js`).catch((e) => {
-    // eslint-disable-next-line no-console
-    console.info(e);
+    if (process.env.NODE_ENV === 'development') {
+      // eslint-disable-next-line no-console
+      console.info(e);
+    }
 
     return enLocale;
   });
 }
 
 export async function initI18n({ lng, basePath }: { lng: string; basePath: string }) {
+  const fallbackLng = 'en-US';
+  const supportedLanguage = languages.find((language) => language === lng) || fallbackLng;
+
   const i18nextInstance = i18n
     .use(initReactI18next) // passes i18n down to react-i18next
     .use(HttpBackend);
@@ -32,12 +37,12 @@ export async function initI18n({ lng, basePath }: { lng: string; basePath: strin
     (window as any).testI18n = (lng = 'cimode') => i18nextInstance.changeLanguage(lng);
   }
 
-  i18nextInstance.on('languageChanged', (lng) => setDateFnsLocale(lng));
-  await setDateFnsLocale(lng);
+  i18nextInstance.on('languageChanged', (newLanguage) => setDateFnsLocale(newLanguage));
+  await setDateFnsLocale(supportedLanguage);
 
   return i18nextInstance.init({
-    lng,
-    fallbackLng: 'en-US',
+    lng: supportedLanguage,
+    fallbackLng,
     defaultNS: 'messages',
     ns: 'messages',
     load: 'currentOnly',
